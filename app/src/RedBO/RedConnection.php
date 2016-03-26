cd255cac51a17f0780bacc006026d1f0fbb5a1cc
Ikraam Moothian Pillay <kai_x@outlook.com>
2016-03-26 10:24:41 +0400

    no message

--- app/src/RedBO/RedConnection.php
+++ app/src/RedBO/RedConnection.php
@@ -2,8 +2,8 @@
 
 require_once('rb.php');
 
-//RedBeanPHP\Facade::setup('mysql:host=localhost;dbname=quagganbazaar', 'root', ''); //for both mysql or mariaDB
+RedBeanPHP\Facade::setup('mysql:host=localhost;dbname=quagganbazaar', 'root', ''); //for both mysql or mariaDB
 //RedBeanPHP\Facade::setup('mysql:host=23.229.226.96;dbname=guildwars2db', 'kai_admin', 'OperationKai24%');
-RedBeanPHP\Facade::setup('mysql:host=23.229.226.96;dbname=quagganbazaar', 'kai_admin', 'OperationKai24%');
-//RedBeanPHP\Facade::debug(true);
+//RedBeanPHP\Facade::setup('mysql:host=23.229.226.96;dbname=quagganbazaar', 'kai_admin', 'OperationKai24%');
+RedBeanPHP\Facade::debug(true);
 ?>
